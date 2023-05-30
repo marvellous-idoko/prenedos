356586a08915248131678704a7a1875941e8a04f
Idoko Marvellous <idokomarvelous@gmail.com>
2023-05-30 02:36:11 +0100

    updated some design 30 - 5 - 23

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,11 @@ let dbt ;
     // dbt = await db.findById('630d4d987a298eb00f946dbb')
 })();
 module.exports = async function fd(){
+  console.log('called . . . .')
       // prod 
   dbt = await db.findById('630d2084647428d2d665306f')
+  console.log(dbt)
+
   // local
     // dbt = await db.findById('630d4d/987a298eb00f946dbb')
      
